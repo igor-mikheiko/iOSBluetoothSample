41f1575e41135ee6c3d7ee39ac8475bbc2fb2be2
Alex Rudyak <al.rudyak@gmail.com>
2015-09-14 12:50:09 +0300

    Initial Commit

---
+++ bluetooth-discovering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bluetooth-discovering
+//
+//  Created by Alex Rudyak on 9/14/15.
+//  Copyright © 2015 *instinctools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
